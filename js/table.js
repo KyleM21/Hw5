9f4ddba101f9996d4cac31756e3eef45e8a45a7d
KyleM21 <44535421+KyleM21@users.noreply.github.com>
2020-07-28 22:46:15 -0400

    Update table.js

--- js/table.js
+++ js/table.js
@@ -7,27 +7,31 @@ function math() {
 	its value it checks it against letters and special characters to ensure 
 	all input is valid.  If it is not valid it changes a paragraph in the 
 	html file to reflect an error, and promptly breaks the script.
+	
+	The idea to use /^[a-zA-Z0-9- ,_]*$/.test(variable) is from 
+	https://www.codexworld.com/how-to/check-special-characters-using-javascript/
+	I modified it to include period for decimal support
 	*/
 	var x1 = document.getElementById("x1").value;
-		if( x1.toUpperCase() != x1.toLowerCase() || /^[a-zA-Z0-9- ,_]*$/.test(x1) == false){
+		if( x1.toUpperCase() != x1.toLowerCase() || /^[a-zA-Z0-9- ,_.]*$/.test(x1) == false){
 			x1  = 0;
 			document.getElementById("error").innerHTML = "Invalid Input! Please try entering a number";
 			throw new Error("Invalid Input");
 		}
 	var x2 = document.getElementById("x2").value;
-		if( x2.toUpperCase() != x2.toLowerCase() || /^[a-zA-Z0-9- ,_]*$/.test(x2) == false){
+		if( x2.toUpperCase() != x2.toLowerCase() || /^[a-zA-Z0-9- ,_.]*$/.test(x2) == false){
 			x2  = 0;
 			document.getElementById("error").innerHTML = "Invalid Input! Please try entering a number";
 			throw new Error("Invalid Input");
 		}
 	var y1 = document.getElementById("y1").value;
-		if( y1.toUpperCase() != y1.toLowerCase() || /^[a-zA-Z0-9- ,_]*$/.test(y1) == false){ 
+		if( y1.toUpperCase() != y1.toLowerCase() || /^[a-zA-Z0-9- ,_.]*$/.test(y1) == false){ 
 			y1 = 0;
 			document.getElementById("error").innerHTML = "Invalid Input! Please try entering a number";
 			throw new Error("Invalid Input");
 		}
 	var y2 = document.getElementById("y2").value;
-		if( y2.toUpperCase() != y2.toLowerCase() || /^[a-zA-Z0-9- ,_]*$/.test(y2) == false){
+		if( y2.toUpperCase() != y2.toLowerCase() || /^[a-zA-Z0-9- ,_.]*$/.test(y2) == false){
 			y2  = 0;
 			document.getElementById("error").innerHTML = "Invalid Input! Please try entering a number";
 			throw new Error("Invalid Input");
@@ -42,8 +46,61 @@ function math() {
 		document.getElementById("error").innerHTML = "Invalid Input! Please switch your y values";
 		throw new Error("Invalid Input");
 	}
-	
 	/*This here is a solution to the table acting strange.  The first column and row of multiplication
+	would just disappear. subtracing 1 from each means the missing row and column appear and function
+	normally
+	*/
+	y1--;
+	x1--;
+	
+	/*This temporary variable holds the tbody with the new table, effectively replacing the old one by 
+	adding it in following the old ones removal
+	*/
+	var temp="";
+	
+	//Checks for big mode
+	if (bigMode.checked == true){
+		temp+="<table id='table'; border='100px'; bordercolor='coral';>";
+	}
+	else{
+		temp+="<table id='table'; border='10px'; bordercolor='blue';>";
+	}
+
+	//This first loop creates the rows, and uses a loop to fill the columns
+	for(var i = y1; i <= y2; i++){
+		
+		temp+="<tr id=row style='height:30px;'>";
+		//This loops builds the rest of the table
+		for(var j = x1; j <= x2; j++){
+
+			if(j == x1){
+				color = "lightgrey";	
+				
+				//This checks if its the first cell of the first row and empties it to provide a cleaner look
+				if(i==y1){
+					color = "lightgrey";
+					temp+="<td style='width:30px; background-color:" + color + "'>" + " " + "</td>";
+				}
+				else{
+				temp+="<td style='width:30px; background-color:" + color + "'>" + (i*1).toFixed(2); + "</td>";
+				}
+			}
+			else if(i == y1){
+				color = "lightgrey";
+				temp+="<td style='width:30px; background-color:" + color + "'>" + (1*j).toFixed(2); + "</td>";
+			}	
+			else {
+				color = "white";
+				temp+="<td style='width:30px; background-color:" + color + "'>" + (i*j).toFixed(2); + "</td>";
+			}
+		}
+		temp+="</tr>";
+	}
+	//This builds the end of the table, appends it.
+	temp+="</table>";
+	$("#rTable").append(temp);
+}
+	
 	would just disappear. subtracing 1 from each means the missing row and column appear and function
 	normally
 	*/
